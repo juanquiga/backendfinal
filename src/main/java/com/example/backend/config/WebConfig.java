c8eff5dbf6db4fa622f4496be1f532212d24952b
juanquiga <juanquiga@unisabana.edu.co>
2025-11-19 14:28:53 -0500

    Cleanup: removed frontend from static

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
       .allowedOrigins(
-         "https://TU_USUARIO.github.io",     // reemplaza por tu GitHub Pages si lo usas
+         "https://juanquiga.github.io",     // reemplaza por tu GitHub Pages si lo usas
          "http://localhost:5500",            // opcional: para pruebas locales
          "https://backendfinal-rkrx.onrender.com" // opcional si frontend alguna vez se sirve desde otro dominio
       )
